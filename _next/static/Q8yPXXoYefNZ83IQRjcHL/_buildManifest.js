4cfdb1c716c8e7e338e2f60f74a9bd311eab331e
nino-ux <nikita.novik.ux@gmail.com>
2023-04-30 20:47:30 +0300

    Update

---
+++ _next/static/Q8yPXXoYefNZ83IQRjcHL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b00ad76398c22692.css","static/chunks/pages/index-73228656cc30d5a0.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- static/fiyv_ukztkKulLUh7Y_pi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b00ad76398c22692.css","static/chunks/pages/index-edc289247b537bb8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
